bd8c77c5459c393b0b726a4b6b307bb9969e6dd4
Fearless18 <badfan18@yahoo.com.ar>
2023-10-10 18:39:53 -0300

    Sección Plantas - un poco de sección Recetas
    Se cambió la forma en mostrar las diferentes plantas y su información.
    Y en la sección Recetas, empecé con una alternativa para mostrarlas.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function mostrarTexto(text) {
+    document.getElementById('text-container').innerHTML = text;
+
+  }
+
+const items = document.querySelectorAll('.item');
+
+items.forEach(item => {
+    item.addEventListener('click', () => {
+        // Quitar la clase 'active' de todos los elementos
+        items.forEach(i => i.classList.remove('active'));
+
+        // Agregar la clase 'active' al elemento clickeado
+        item.classList.add('active');
+    });
+});
